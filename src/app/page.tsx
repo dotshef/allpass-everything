dd29e27bb5eae917d80de09969bf880ab74243ab
sijun <sionwer5@gmail.com>
2026-03-08 14:31:10 +0900

    fix: 폰트 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -31,12 +31,12 @@ export default function Home() {
 
             <div className="mb-8">
               <div className="space-y-3 text-xl font-semibold leading-tight text-black [text-shadow:_2px_2px_8px_rgba(255,255,255,0.8)]">
-                <div>-. SK하이닉스, 현대로템 출신 취업 전문 컨설턴트</div>
-                <div>-. 연 1,000건+ 자소서 첨삭 및 Zoom 비대면 면접 컨설팅</div>
-                <div>-. 현재까지 누적 990명+ 최종합격 이력</div>
-                <div>-. 숨고, 크몽, 블로그, 당근, 사람인 플랫폼 취업컨설턴트</div>
-                <div>-. 유명 취업 컨설팅 기업 &quot;마스터 강사&quot;</div>
-                <div>-. Thread 취준생 4만 팔로워 보유</div>
+                <div>- SK하이닉스, 현대로템 출신 취업 전문 컨설턴트</div>
+                <div>- 연 1,000건+ 자소서 첨삭 및 Zoom 비대면 면접 컨설팅</div>
+                <div>- 현재까지 누적 990명+ 최종합격 이력</div>
+                <div>- 숨고, 크몽, 블로그, 당근, 사람인 플랫폼 취업컨설턴트</div>
+                <div>- 유명 취업 컨설팅 기업 &quot;마스터 강사&quot;</div>
+                <div>- Thread 취준생 4만 팔로워 보유</div>
               </div>
             </div>
 
